00bb45f40b7e8e76b806c255ae3a0e7bed579d49
hawra <hawra.yusuf02@outlook.com>
2025-09-08 20:10:59 +0300

    signup complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,35 @@
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/sign-up", (req, res) => {
   res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+
+if (userInDatabase) {
+  return res.send("Username or Password is invalid.");
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+}
+
+ const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+ req.body.password = hashedPassword;
+
+ // validation logic
+
+const user = await User.create(req.body);
+res.send(`Thanks for signing up ${user.username}`);
+
+
+  res.send("Form submission accepted!");
 });
 
 
