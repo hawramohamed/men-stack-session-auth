eb6e6ddf76c08652ce4e22d4d6ae5c30cd809cee
hawra <hawra.yusuf02@outlook.com>
2025-09-08 19:46:40 +0300

    add auth controller

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+//controllers
+const authController = require("./controllers/auth.js");
+
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -27,6 +31,9 @@ app.use(morgan('dev'));
 app.get('/', async (req, res) => {
     res.render("index.ejs");
 });
+
+app.use("/auth", authController);
+ 
 
 //PROTECTED
 
